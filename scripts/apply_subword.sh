2e9fa57192bd2662f7d6577662d29d1ac13080f1
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2021-08-19 19:35:09 +0530

    Update apply_subword.sh

--- scripts/apply_subword.sh
+++ scripts/apply_subword.sh
@@ -1,7 +1,7 @@
-# echo "learning bpe and subword"
-# python subword_nmt/subword_nmt/learn_joint_bpe_and_vocab.py --verbose --input indic_preprocess.mr indic_preprocess.en \
-# 	-s 32000 \
-# 	-o code --write-vocabulary vocab.mr vocab.en
+echo "learning bpe and subword"
+python subword_nmt/subword_nmt/learn_joint_bpe_and_vocab.py --verbose --input indic_preprocess.mr indic_preprocess.en \
+	-s 32000 \
+	-o code --write-vocabulary vocab.mr vocab.en
 echo "apply bpe [en]"
 python subword_nmt/subword_nmt/apply_bpe.py -c code \
 	--vocabulary vocab.mr \
